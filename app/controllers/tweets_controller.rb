be9e1f13382e42c3d4dff7ece88db93d89c911ea
Erin Stalzer <erinstalzer@gmail.com>
2019-06-11 19:45:47 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -56,7 +56,7 @@ class TweetsController < ApplicationController
     if logged_in?
     #  binding.pry
       if params["content"] == ""
-        redirect '/tweets/:id/edit'
+        erb :'tweets/edit'
       else
         @tweet = Tweet.find_by(id: params[:id])
         @tweet.update(content: params["content"])
